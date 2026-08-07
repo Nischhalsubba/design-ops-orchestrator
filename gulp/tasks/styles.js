c44b42a58827e1a7cdd6254bb50d11eb5f178b2c
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-07 11:54:21 +0545

    Remove vulnerable desktop notification wrapper

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,7 +5,6 @@ import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import plumber from 'gulp-plumber';
-import notify from 'gulp-notify';
 import rename from 'gulp-rename';
 import gulpIf from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
@@ -33,14 +32,14 @@ export const styles = () => {
     return gulp.src(config.paths.src.styles)
         .pipe(plumber({
             errorHandler: async function(err) {
-                notify.onError("Error: <%= error.message %>")(err);
+                console.error(`Styles build error: ${err.message}`);
                 await healBuildError('Styles', err);
                 this.emit('end');
             }
         }))
         .pipe(cached('styles'))
         .pipe(dependents())
-        .pipe(sourcemaps.init()) // Always init source maps
+        .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: ['node_modules'],
             outputStyle: 'expanded'
@@ -48,39 +47,28 @@ export const styles = () => {
         .pipe(groupMedia())
         .pipe(postcss([
             postcssAssets({ loadPaths: ['src/assets/img'] }),
-            postcssPresetEnv({ stage: 1 }), 
+            postcssPresetEnv({ stage: 1 }),
             postcssSort({ "properties-order": "alphabetical" }),
-            postcssPxtorem({ propList: ['*'] }), 
+            postcssPxtorem({ propList: ['*'] }),
             autoprefixer(),
         ]))
         .pipe(header(config.banner, { pkg : pkg } ))
-        
-        // --- Write Base CSS ---
         .pipe(gulp.dest(config.paths.dist.css))
-        
-        // --- Versioning (Production) ---
         .pipe(gulpIf(config.isProduction, rev()))
-        .pipe(gulpIf(config.isProduction, revDel())) // Remove un-hashed file
+        .pipe(gulpIf(config.isProduction, revDel()))
         .pipe(gulp.dest(config.paths.dist.css))
-        
-        // --- RTL Generation ---
         .pipe(rtlcss())
         .pipe(rename({ suffix: '-rtl' }))
         .pipe(gulp.dest(config.paths.dist.css))
-        
-        // --- Minification & Compression (Production) ---
         .pipe(gulpIf(config.isProduction, postcss([cssnano()])))
-        .pipe(gulp.dest(config.paths.dist.css)) // Save minified
-        
+        .pipe(gulp.dest(config.paths.dist.css))
         .pipe(gulpIf(config.isProduction, brotli.compress({
             extension: 'br',
             skipLarger: true,
-            mode: 1, 
+            mode: 1,
             quality: 11
         })))
         .pipe(gulpIf(config.isProduction, gulp.dest(config.paths.dist.css)))
-
-        // --- Source Maps (Write to .map files) ---
         .pipe(sourcemaps.write('.'))
         .pipe(size({ title: 'Styles', gzip: true }));
 };
