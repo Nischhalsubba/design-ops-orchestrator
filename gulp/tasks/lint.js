531557afac30755f8b950b7edac3cb888aa6917a
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-07 10:40:05 +0545

    Use direct ESLint and Pug lint CLIs

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -2,10 +2,29 @@ import { spawn } from 'node:child_process';
 import gulp from 'gulp';
 import { globSync } from 'glob';
 import stylelint from 'stylelint';
-import eslint from 'gulp-eslint';
-import plumber from 'gulp-plumber';
-import notify from 'gulp-notify';
 import { config } from '../config.js';
+
+const runLocalCommand = (command, files, label) => {
+    if (files.length === 0) {
+        return Promise.resolve();
+    }
+
+    return new Promise((resolve, reject) => {
+        const executable = process.platform === 'win32'
+            ? `node_modules\\.bin\\${command}.cmd`
+            : `node_modules/.bin/${command}`;
+        const child = spawn(executable, files, { stdio: 'inherit' });
+
+        child.on('error', reject);
+        child.on('close', code => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(new Error(`${label} failed with exit code ${code}`));
+            }
+        });
+    });
+};
 
 // Lint SCSS
 export const lintStyles = async () => {
@@ -20,37 +39,16 @@ export const lintStyles = async () => {
     }
 };
 
-// Lint TypeScript/JS
+// Lint TypeScript/JS with the project's direct ESLint dependency.
 export const lintScripts = () => {
-    return gulp.src(config.paths.src.scripts)
-        .pipe(plumber({ errorHandler: notify.onError("ESLint Error: <%= error.message %>") }))
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+    const files = globSync(config.paths.src.scripts);
+    return runLocalCommand('eslint', files, 'ESLint');
 };
 
 // Lint Pug without the obsolete gulp-pug-linter wrapper.
 export const lintPug = () => {
     const files = globSync(config.paths.src.markup);
-    if (files.length === 0) {
-        return Promise.resolve();
-    }
-
-    return new Promise((resolve, reject) => {
-        const executable = process.platform === 'win32'
-            ? 'node_modules\\.bin\\pug-lint.cmd'
-            : 'node_modules/.bin/pug-lint';
-        const child = spawn(executable, files, { stdio: 'inherit' });
-
-        child.on('error', reject);
-        child.on('close', code => {
-            if (code === 0) {
-                resolve();
-            } else {
-                reject(new Error(`Pug lint failed with exit code ${code}`));
-            }
-        });
-    });
+    return runLocalCommand('pug-lint', files, 'Pug lint');
 };
 
 export const lint = gulp.parallel(lintStyles, lintScripts, lintPug);
