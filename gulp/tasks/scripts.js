775132bcc3518e98a64504c9e08601b1bf9b0f74
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-07 11:54:36 +0545

    Remove notification wrapper from script build

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,7 +1,6 @@
 import gulp from 'gulp';
 import esbuild from 'gulp-esbuild';
 import plumber from 'gulp-plumber';
-import notify from 'gulp-notify';
 import size from 'gulp-size';
 import header from 'gulp-header';
 import gulpIf from 'gulp-if';
@@ -20,7 +19,7 @@ export const scripts = () => {
     return gulp.src(config.paths.src.scripts)
         .pipe(plumber({
             errorHandler: async function(err) {
-                notify.onError("Error: <%= error.message %>")(err);
+                console.error(`Scripts build error: ${err.message}`);
                 await healBuildError('Scripts', err);
                 this.emit('end');
             }
@@ -30,29 +29,22 @@ export const scripts = () => {
             target: 'es2022',
             bundle: true,
             minify: false,
-            sourcemap: false, // We use gulp-sourcemaps instead
+            sourcemap: false,
             platform: 'browser',
         }))
         .pipe(header(config.banner, { pkg : pkg } ))
-        
-        // --- Minification (Production) ---
         .pipe(gulpIf(config.isProduction, terser({
              compress: { drop_console: true }
         })))
-        
-        // --- Versioning (Production) ---
         .pipe(gulpIf(config.isProduction, rev()))
         .pipe(gulpIf(config.isProduction, revDel()))
         .pipe(gulp.dest(config.paths.dist.js))
-        
-        // --- Compression (Production) ---
         .pipe(gulpIf(config.isProduction, brotli.compress({
             extension: 'br',
             skipLarger: true,
             quality: 11
         })))
         .pipe(gulpIf(config.isProduction, gulp.dest(config.paths.dist.js)))
-        
         .pipe(sourcemaps.write('.'))
         .pipe(size({ title: 'Scripts', gzip: true }));
 };
