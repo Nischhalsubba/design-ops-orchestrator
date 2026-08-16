0c6812fc8e7f5ffd946f79da7405bf388ae659bd
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-16 17:04:58 +0545

    fix: restore style lint watch task

--- app/gulp/tasks/lint.js
+++ app/gulp/tasks/lint.js
@@ -3,22 +3,32 @@ import eslint from 'gulp-eslint-new';
 import pugLinter from 'gulp-pug-linter';
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
+import stylelint from 'stylelint';
 import { config } from '../config.js';
 
-// Lint TypeScript/JS
+export const lintStyles = async () => {
+    const result = await stylelint.lint({
+        files: config.paths.src.styles,
+        configFile: 'stylelint.config.mjs',
+        formatter: 'string',
+    });
+
+    if (result.report) console.log(result.report);
+    if (result.errored) throw new Error('Stylelint reported errors');
+};
+
 export const lintScripts = () => {
     return gulp.src(config.paths.src.scripts)
-        .pipe(plumber({ errorHandler: notify.onError("ESLint Error: <%= error.message %>") }))
+        .pipe(plumber({ errorHandler: notify.onError('ESLint Error: <%= error.message %>') }))
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 };
 
-// Lint Pug
 export const lintPug = () => {
     return gulp.src(config.paths.src.markup)
-        .pipe(plumber({ errorHandler: notify.onError("PugLint Error: <%= error.message %>") }))
+        .pipe(plumber({ errorHandler: notify.onError('PugLint Error: <%= error.message %>') }))
         .pipe(pugLinter({ reporter: 'default' }));
 };
 
-export const lint = gulp.parallel(lintScripts, lintPug);
+export const lint = gulp.parallel(lintStyles, lintScripts, lintPug);
